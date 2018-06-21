1e19d3537f6d85f639f9bc69f56c7fe5ee46988e
Janusz Szubert <theinvalidnonce@gmail.com>
2018-06-21 10:34:24 -0400

    Update 2nd problem answer

--- 2.js
+++ 2.js
@@ -1,5 +1,3 @@
-
-
 // Question 2 -- decodeString(s): Given an encoded string, return its corresponding decoded string. 
 
 // The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is repeated exactly k times. Note: k is guaranteed to be a positive integer. 
@@ -7,14 +5,16 @@
 // For s = "4[ab]", the output should be decodeString(s) = "abababab" 
 // For s = "2[b3[a]]", the output should be decodeString(s) = "baaabaaa"
 
-const decodeString = s => {
+const decodeString = str => {
 	//regex allows you to capture innermost group
 	let regexRule = /(\d*)(?:\[)([a-z]*)(?:\])/
 
-
-
-
-	// return s
+  for (let i = 0; i < str.length; i++) {
+    if (str.includes('[')) {
+    	str = str.replace(regexRule, (match, amount, part) => part.repeat(amount));
+  }
+}
+  return str
 }
 
 let a = '4[ab]'
